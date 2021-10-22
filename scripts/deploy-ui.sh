a0c5b4e25128d6452835b7a432b35492067d9a9d
Ardeshir Eshghi <ardi.eshghi@gmail.com>
2021-10-22 11:37:17 +0100

    Fixes deploy ui script

--- scripts/deploy-ui.sh
+++ scripts/deploy-ui.sh
@@ -7,6 +7,12 @@ env="${1:-dev}"
 export REACT_APP_API_URL="https://taboo-api-$env.uksouth.cloudapp.azure.com"
 
 yarn install
+
+if [[ ! -f ".env" ]]; then
+    cp .env{.example,}
+fi
+
+sed -i "s#{{apiBaseUrl}}#$REACT_APP_API_URL#g" .env
 yarn build
 
 wrangler publish --env "$env"
