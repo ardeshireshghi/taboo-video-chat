fc3fe18f84029d2e48171314770f25a262be34d8
Ardeshir Eshghi <ardi.eshghi@gmail.com>
2021-10-25 14:48:33 +0100

    Adds health check endpoint to service

--- api/lib/infrastructure/webserver.ts
+++ api/lib/infrastructure/webserver.ts
@@ -11,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get('/health', (req, res) => {
+  res.send('OK');
+});
+
 app.use('/', router);
 app.use(errorHandler);
 
